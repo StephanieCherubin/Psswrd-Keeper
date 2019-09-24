9bbaede7bef6754802dfc97fce979886c8c19e58
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-09-24 12:21:54 -0700

    add controlled input

--- src/Password.js
+++ src/Password.js
@@ -4,6 +4,7 @@ class Password extends Component {
   constructor(props) {
     super(props)
     this.state = { password: 'p@$$w0rd'}
+    this.state = { description: ''}
   }
 
   generatePassword() {
@@ -14,12 +15,33 @@ class Password extends Component {
   render() {
     return (
       <div>
-        <div>{this.state.password}</div>
+        <div>
+          <label>Unique description of password:
+            <input
+                type="text" name="description"
+                value={this.state.description}
+                onChange={(e) => {this.setState({ description: e.target.value })}}
+              />
+          </label>
+            <br />
+          <label>
+            Password:
+            <input
+              type="text" name="password"
+              value={this.state.password}
+              onChange={(e) => {this.setState({ password: e.target.value })}}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </div>
+        {/* <div>{this.state.password}</div> */}
+
         <div>
           <button onClick={(e) => {
             this.generatePassword()
           }}>Generate</button>
         </div>
+
       </div>
     )
   }
